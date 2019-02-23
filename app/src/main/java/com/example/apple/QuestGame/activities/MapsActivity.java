c28d1b7f00b6b253bd778e5f1292b35fce74046d
harutus22 <harutus22@yahoo.com>
2019-02-24 03:27:50 +0400

    Sign up and sign in added

--- app/src/main/java/com/example/apple/QuestGame/activities/MapsActivity.java
+++ app/src/main/java/com/example/apple/QuestGame/activities/MapsActivity.java
@@ -7,6 +7,8 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.provider.Settings;
 import android.support.annotation.NonNull;
@@ -21,12 +23,16 @@ import com.example.apple.QuestGame.R;
 import com.example.apple.QuestGame.utils.Constants;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -34,6 +40,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private GoogleMap mMap;
     private boolean mapType = false;
     private boolean locationPermission = false;
+    private FusedLocationProviderClient fusedLocationProviderClient;
+    private LocationManager locationManager;
+    private LocationListener locationListener;
 
 
     @Override
@@ -42,8 +51,25 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         setContentView(R.layout.activity_maps);
         getLocationPermission();
         initMap();
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+    }
 
-
+    private void getLastKnownLocation() {
+//        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+//                != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this,
+//                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+//            return;
+//        }
+//        fusedLocationProviderClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
+//            @Override
+//            public void onComplete(@NonNull Task<Location> task) {
+//                if(task.isSuccessful()){
+//                    Location location = task.getResult();
+//                    double latitude = location.getLatitude();
+//                    double longitude = location.getLongitude();
+//                }
+//            }
+//        });
     }
 
     private void initMap() {
@@ -67,17 +93,39 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
         if(mapType){
             mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
         } else {
             mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
         }
+        locationManager = (LocationManager) this.getSystemService(LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+            }
 
-        LatLng aca = new LatLng(40.198887912292534, 44.490739703178406);
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+
+        LatLng aca = new LatLng(40.198887912292537, 44.490739703178408);
         mMap.addMarker(new MarkerOptions().position(aca).title("Marker in ACA"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(aca));
-        mMap.animateCamera(CameraUpdateFactory.zoomTo(15));
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(aca));
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(13));
         setMyLOcation();
     }
 
@@ -97,6 +145,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         if(checkMapServices()){
             if(locationPermission){
                 getQuests();
+                getLastKnownLocation();
             } else {
                 getLocationPermission();
             }
@@ -108,6 +157,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 PackageManager.PERMISSION_GRANTED) {
             locationPermission = true;
             getQuests();
+            getLastKnownLocation();
         } else {
             ActivityCompat.requestPermissions(this, new String[]{Constants.FINE_LOCATION},
                     Constants.LOCATION_REQUEST_CODE);
@@ -164,6 +214,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             case Constants.ENABLE_GPS_CODE: {
                 if (locationPermission) {
                     getQuests();
+                    getLastKnownLocation();
                 } else {
                     getLocationPermission();
                 }
