b5ef6467aaab61bd535761af49ee117b123ac0f6
6gag21 <38590564+6gag21@users.noreply.github.com>
2019-03-27 22:19:11 +0400

    AR

---
+++ app/src/main/java/com/example/apple/QuestGame/activities/ArActivity.java
@@ -0,0 +1,191 @@
+package com.example.apple.QuestGame.activities;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import com.example.apple.QuestGame.R;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.wikitude.architect.ArchitectStartupConfiguration;
+import com.wikitude.architect.ArchitectView;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+
+public class ArActivity extends AppCompatActivity {
+
+    private static final int REQUEST_FINE_LOCATION = 200;
+    ArchitectView mArchitectView;
+
+    private FusedLocationProviderClient mFusedLocationClient;
+    private LocationRequest mLocationRequest;
+    private LocationCallback mLocationCallback;
+
+    private static final int MY_CAMERA_REQUEST_CODE = 100;
+
+
+
+    @SuppressLint("MissingPermission")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ar);
+        checkPermissions();
+        mArchitectView = findViewById(R.id.architectView);
+
+        getLocation();
+
+
+        final ArchitectStartupConfiguration config = new ArchitectStartupConfiguration();
+        config.setFeatures(ArchitectStartupConfiguration.Features.Geo);
+        config.setLicenseKey(getString(R.string.wikitude_license_key));
+        mArchitectView.onCreate(config);
+
+
+        final JSONArray jsonArray = generatePoiInformation();
+
+
+        mArchitectView.callJavascript("World.createModelAtLocation(" + jsonArray.toString() + ")");
+
+    }
+
+    private void getLocation() {
+        mLocationRequest = LocationRequest.create()
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                .setInterval(30 * 1000)
+                .setFastestInterval(5 * 1000);
+
+        mLocationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                for (Location location : locationResult.getLocations()){
+                    mArchitectView.setLocation(location.getLatitude(), location.getLongitude(), location.getAccuracy());
+                }
+            }
+        };
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        mFusedLocationClient.requestLocationUpdates(mLocationRequest, mLocationCallback, null);
+    }
+
+
+    @SuppressLint("MissingPermission")
+    @Override
+
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode){
+            case MY_CAMERA_REQUEST_CODE:
+            {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "camera permission granted", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(this, "camera permission denied", Toast.LENGTH_LONG).show();
+                }
+                break;
+            }
+            case REQUEST_FINE_LOCATION:
+                if (grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                    mFusedLocationClient.getLastLocation().addOnSuccessListener(new OnSuccessListener<Location>() {
+                        @Override
+                        public void onSuccess(Location location) {
+                            mArchitectView.setLocation(location.getLatitude(), location.getLongitude(), location.getAccuracy());
+                        }
+                    });
+                }
+        }
+    }
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mArchitectView.onPostCreate();
+        try {
+            mArchitectView.load("3dModelAtGeo/index.html");
+        } catch (IOException e) {
+            Toast.makeText(this, getString(R.string.error_loading_ar_experience), Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mArchitectView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mArchitectView.onPause();
+}
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mArchitectView.clearCache();
+        mArchitectView.onDestroy();
+        if (mFusedLocationClient != null) {
+            mFusedLocationClient.removeLocationUpdates(mLocationCallback);
+        }
+
+    }
+
+    private static JSONArray generatePoiInformation() {
+
+        final JSONArray coins = new JSONArray();
+
+        final String ATTR_LATITUDE = "latitude";
+        final String ATTR_LONGITUDE = "longitude";
+
+        final HashMap<String, String> coinInformation = new HashMap<>();
+
+        double[] coinLocationLatLon = new double[]{40.201125, 44.494802};
+        coinInformation.put(ATTR_LATITUDE, String.valueOf(coinLocationLatLon[0]));
+        coinInformation.put(ATTR_LONGITUDE, String.valueOf(coinLocationLatLon[1]));
+        coins.put(new JSONObject(coinInformation));
+
+
+        return coins;
+    }
+
+    private void checkPermissions() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        REQUEST_FINE_LOCATION);
+            }
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.CAMERA)
+                    != PackageManager.PERMISSION_GRANTED ) {
+                requestPermissions(new String[]{Manifest.permission.CAMERA},
+                        MY_CAMERA_REQUEST_CODE);
+            }
+        }
+    }
+
+}
